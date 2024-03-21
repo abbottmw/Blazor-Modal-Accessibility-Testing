8aea732da01da1d9bc9d7f57af329a6084e2641d
Matthew Abbott <Matthew.Abbott@opm.gov>
2024-03-21 14:43:23 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ModalAccessibilityTest;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddBlazoredModal();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿Simple Test code testing Accessibility with Modal windows.
+
+Things being tested.
+
+ 1. Screen Reader reads Modal Window
+ 2. Focus returns to button that triggered it when closed
+ 3. Focus Trapping
+
+Results are  Blazored.Modal is not being read by [NVDA](https://www.nvaccess.org/) (screen reader tested with).  
+Also other issues with focus trap and custom layouts.
